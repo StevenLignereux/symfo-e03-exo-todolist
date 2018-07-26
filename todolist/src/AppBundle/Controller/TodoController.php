474b45cc572d75e26c945d3896ad26ca003c8cda
Steven Lignereux <steven.lignereux@hotmail.fr>
2018-07-26 04:03:58 +0200

    home page

--- todolist/src/AppBundle/Controller/TodoController.php
+++ todolist/src/AppBundle/Controller/TodoController.php
@@ -6,16 +6,14 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
+class TodoController extends Controller
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="todo_home")
      */
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('todo/index.html.twig');
     }
 }
