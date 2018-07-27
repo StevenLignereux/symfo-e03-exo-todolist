1be47bec72ac5833593413f00a2ecda68d0fb732
Steven Lignereux <steven.lignereux@hotmail.fr>
2018-07-27 22:29:59 +0200

    home and list 1:21:43

--- todolist/src/AppBundle/Controller/TodoController.php
+++ todolist/src/AppBundle/Controller/TodoController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Model\TodoModel;
 
 
 class TodoController extends Controller
@@ -12,7 +13,7 @@ class TodoController extends Controller
     /**
      * @Route("/", name="todo_home")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
         // replace this example code with whatever you need
         return $this->render('todo/index.html.twig');
@@ -23,6 +24,9 @@ class TodoController extends Controller
      */
     public function listAction()
     {
-      return $this->render('todo/list.html.twig');
+      $todos = TodoModel::findAll();
+      return $this->render('todo/list.html.twig', [
+        'todos' => $todos,
+      ]);
     }
 }
